32cad1cdd1023ac6028db10778e7e91886060a16
Keiichi <keiichi.hirano.a@ccbji.co.jp>
2019-07-08 14:00:51 +0900

    Update ddp.py

--- app/processes/ddp.py
+++ app/processes/ddp.py
@@ -14,39 +14,11 @@ class Process(object):
         pass
 
     def DDP_check_process(self,check1,check2,check3,check4,check5):
-        WEEKDAY = [
-            '月曜日',
-            '火曜日',
-            '水曜日',
-            '木曜日',
-            '金曜日',
-            '土曜日',
-            '日曜日'
-        ]
-        GARBAGE = [
-            '燃えるごみ',
-            'プラごみ',
-            '燃えるごみ',
-            'その他プラごみ',
-            '燃えるごみ',
-            '資源ごみ',
-            'なし'
-        ]
-        DETAIL = {
-            '燃えるごみ': '生ごみ、紙くず',
-            'プラごみ': '容器包装プラ、リサイクルプラ',
-            'その他プラごみ': '30cm未満のプラやゴム、合成皮革',
-            '資源ごみ': 'ビン、缶、金属、かさ、小型家電、布',
-            '陶磁器/ガラスごみ': '刃物、30cm以上50cm未満のプラ',
-            'なし': 'なし'
-        }
-        date_time = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-        weekday = date_time.weekday()
-        week_number = self._get_week_number(date_time)
-        if weekday == 2 and week_number == 4:
-            return '第4{}は「{}」の日です\n({})'.format(WEEKDAY[weekday], '陶磁器/ガラスごみ', DETAIL.get('陶磁器/ガラスごみ'))
-        else:
-            return '{}は「{}」の日です\n({})'.format(WEEKDAY[weekday], GARBAGE[weekday], DETAIL.get(GARBAGE[weekday]))
+#        if weekday == 2 and week_number == 4:
+#            return '第4{}は「{}」の日です\n({})'.format(WEEKDAY[weekday], '陶磁器/ガラスごみ', DETAIL.get('陶磁器/ガラスごみ'))
+#        else:
+#            return '{}は「{}」の日です\n({})'.format(WEEKDAY[weekday], GARBAGE[weekday], DETAIL.get(GARBAGE[weekday]))
+            return '{}は「{}」です\n({})'.'テスト','良好','もうひと息！'
 
     def _get_week_number(self, date_time):
         day = date_time.day

8cfd57506c1d8af5f1b66eedba46fcf17bc8d5a2
Keiichi <keiichi.hirano.a@ccbji.co.jp>
2019-07-08 15:50:06 +0900

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -94,4 +94,6 @@ def postback():
 if __name__ == '__main__':
     host = os.getenv('HOST', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
-    app.run(host=host, port=port, debug=True)
+#    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
+#

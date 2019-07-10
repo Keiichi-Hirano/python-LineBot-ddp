2094e7590e060009a6096d8149878e598d0257fb
Keiichi-Hirano <keiichi.hirano.a@ccbji.co.jp>
2019-07-10 21:40:03 +0900

    Update story_board.py

--- app/framework/nslinebot/models/story_board.py
+++ app/framework/nslinebot/models/story_board.py
@@ -22,7 +22,7 @@ class StoryBoard(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def story_board(self, text):
+    def story_board(self, check_T, text):
         pass
 
     def get_message(self, text):

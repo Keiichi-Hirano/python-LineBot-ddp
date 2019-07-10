03dd1769e63c918824dcd115100c13b7829ed008
Keiichi-Hirano <keiichi.hirano.a@ccbji.co.jp>
2019-07-10 21:22:43 +0900

    Update story_board.py

--- app/framework/nslinebot/models/story_board.py
+++ app/framework/nslinebot/models/story_board.py
@@ -29,6 +29,6 @@ class StoryBoard(metaclass=abc.ABCMeta):
         logger.info('get_message:{}'.format(text))
         return TextSendMessage(text=text)
 
-    def get_template(self, scene, text=None, check_t):
+    def get_template(self, scene, text=None, check_T):
         logger.info('get_template:{}'.format(scene))
         return self.story_board(text).get(scene, None)

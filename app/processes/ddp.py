eb7331f5817ab7840367c35d6f7838ce108f5e5c
Keiichi-Hirano <keiichi.hirano.a@ccbji.co.jp>
2019-07-29 16:29:34 +0900

    Update ddp.py

--- app/processes/ddp.py
+++ app/processes/ddp.py
@@ -10,30 +10,30 @@ logger = logging.getLogger(__name__)
 
 # DMBS 
 DB_answer = ''
-RDBMS = 'RDBMS使用の必須要件が発生した場合に、RDBMS(Oracle/DB2/PostgreSQL)を使用\n'
-MarkLogic  = 'MarklogicをDataHUBとして使用\n'
-Hadoop  = 'Data Aggregation(集計)データの格納を目的にDWHとしてHadoopを使用\n'
-HANA  = 'CokeOne Dataをリアルタイムにレポート・分析する際にHANAを使用\n'
-CokeOne  = 'CokeOneトランザクションの更新を伴う場合は、CokeOneシステムを使用\n'
+RDBMS = 'RDBMS使用の必須要件が発生した場合に、RDBMS(Oracle/DB2/PostgreSQL)を使用'
+MarkLogic  = 'MarklogicをDataHUBとして使用'
+Hadoop  = 'Data Aggregation(集計)データの格納を目的にDWHとしてHadoopを使用'
+HANA  = 'CokeOne Dataをリアルタイムにレポート・分析する際にHANAを使用'
+CokeOne  = 'CokeOneトランザクションの更新を伴う場合は、CokeOneシステムを使用'
 # Business Logic
 Logic_answer = ''
-Abinito = 'ETLに関わるすべての処理を担うプラットフォームとしてAbInitoを使用(複数データの非同期更新）\n'
-JAVA = 'API及び、データエントリーに関わるGUIの開発にてJAVA/Java Scriptを使用(少量データの即時同期更新)\n'
-Python = '統計解析・分析・シュミレーション処理開発にPython及びRを使用※JAVA代替としも使用可能\n'
-ABAP = 'CokeOne・HANAを始めとするSAP環境では、専用開発言語のABAPを使用\n'
+Abinito = 'ETLに関わるすべての処理を担うプラットフォームとしてAbInitoを使用(複数データの非同期更新）'
+JAVA = 'API及び、データエントリーに関わるGUIの開発にてJAVA/Java Scriptを使用(少量データの即時同期更新)'
+Python = '統計解析・分析・シュミレーション処理開発にPython及びRを使用※JAVA代替としも使用可能'
+ABAP = 'CokeOne・HANAを始めとするSAP環境では、専用開発言語のABAPを使用'
 
 # Presentation
 Pre_answer = ''
 BI_tool = '分析用にAggregation(集計)されたデータを元に' + \
 'データを可視化(Visualization)分析を行う際にTableauまたは、Sisenseを使用' + \
-'※可視化のパターン変化が多い場合、継続して使用可能\n'
+'※可視化のパターン変化が多い場合、継続して使用可能'
 UI5 = 'SAP HANA上でのレポートを行う際にUI5(SAPのHTMLベースGUI)を使用'
 HTML5 = 'HTML5：標準化選定にてCokeOne以外のシステムはGUIをHTML5で構築する為' + \
 'UIを使用の際はHTML5を使用\n' + \
 'D3:Tableauで可視化(Visualization)されたものをHTMLベースで再構築する際に使用' + \
-'※可視化のパターン変化が少ない、又は、レポートの代替機能構築時使用\n'
-SAP_GUI = 'SAP専用GUIを使用\n'
-Export_File = 'UIの構築を伴わない場合、File Exportを実装(AbInito)\n'
+'※可視化のパターン変化が少ない、又は、レポートの代替機能構築時に使用\n'
+SAP_GUI = 'SAP専用GUIを使用'
+Export_File = 'UIの構築を伴わない場合、File Exportを実装(AbInito)'
 
 class Process(object):
     def __init__(self):
@@ -66,14 +66,14 @@ class Process(object):
                         Pre_answer = Export_File                               
 #                   Analytics
                     if check5 == 'Y':
-                        DB_answer = DB_answer + 'また' + Hadoop
-                        Logic_answer = Logic_answer + 'また' + Python
+                        DB_answer = DB_answer + '\nまた' + Hadoop
+                        Logic_answer = Logic_answer + '\nまた' + Python
 #                   Non-Analytics
                     else:                
                         pass
 #                   Use UI + Analytics
                     if check3 == 'Y' and check5 == 'Y':     
-                        Pre_answer = Pre_answer + 'また' + BI_tool
+                        Pre_answer = Pre_answer + '\nまた' + BI_tool
             else:
 #           CokeOne CRUD
                 DB_answer = CokeOne
@@ -86,7 +86,7 @@ class Process(object):
                     Pre_answer = Export_File
 #       CokeOne Transaction以外
         else:
-            DB_answer = RDBMS + 'また' + MarkLogic
+            DB_answer = RDBMS + '\nまた' + MarkLogic
             Logic_answer = Abinito               
 #           Use UI
             if check3 == 'Y':
@@ -96,19 +96,19 @@ class Process(object):
                 Pre_answer = Export_File                               
 #           Analytics
             if check5 == 'Y':
-                DB_answer = DB_answer + 'また' + Hadoop
-                Logic_answer = Logic_answer + 'また' + Python
+                DB_answer = DB_answer + '\nまた' + Hadoop
+                Logic_answer = Logic_answer + '\nまた' + Python
 #           Non-Analytics
             else:                
                 pass
 #           Use UI + Analytics
             if check3 == 'Y' and check5 == 'Y':     
-                Pre_answer = Pre_answer + 'また' + BI_tool
+                Pre_answer = Pre_answer + '\nまた' + BI_tool
 #           Realtime
             if check4 == 'Y':
-                Logic_answer = Logic_answer + 'また' + JAVA
+                Logic_answer = Logic_answer + '\nまた' + JAVA
 #        return '1は{}・2は{}・3は{}・4は{}・5は{}です\n'.format(check1,check2,check3,check4,check5)
-        return '1.DBは{}\n2.開発言語は{}\n3.プレゼンテーション機能は{}が推奨となります。'.format(DB_answer,Logic_answer,Pre_answer)
+        return '1.DBは{}。\n2.開発言語は{}。\n3.プレゼンテーション機能は{}が推奨となります。'.format(DB_answer,Logic_answer,Pre_answer)
 
 #    def _get_week_number(self, date_time):
 #        day = date_time.day
